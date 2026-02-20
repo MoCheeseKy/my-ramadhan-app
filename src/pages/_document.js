435f9243be6f38483d2301114045948c9d5081c3
Your NameMoCheeseKy <rifkyking04@gmail.com>
2026-02-20 21:31:34 +0700

    BIG UPDATE: Fiqih

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,7 +10,7 @@ export default function Document() {
         <meta name='apple-mobile-web-app-capable' content='yes' />
         <meta name='apple-mobile-web-app-status-bar-style' content='default' />
         <meta name='apple-mobile-web-app-title' content='MyRamadhan' />
-        <link rel='apple-touch-icon' href='/icon-192.png' />
+        <link rel='apple-touch-icon' href='/logo-myramadhan.jpeg' />
 
         {/* Font Arab (Amiri) yang sudah kita bahas sebelumnya */}
         <link
